46eb0ba9c330ec12fdd785cbc07268b3ffd8cf97
SDLahti <79568795+SDLahti@users.noreply.github.com>
2022-02-01 17:35:23 -0700

    Added skeleton of main method and assoc methods
    Added following methods
    
    addItem():
    checkout():
    checkin():
    log():
    remove():
    init():
    verify():

--- bchoc.py
+++ bchoc.py
@@ -1,1 +1,44 @@
+#! /usr/bin/Python3
 
+import util, layout
+import sys, types, time, random, os
+
+def addItem():
+   return 0
+
+def checkout():
+   return 0
+
+def checkin():
+   return 0
+
+def log():
+   return 0
+
+def remove():
+   return 0
+
+def init():
+   return 0
+
+def verify():
+   return 0
+
+
+#Main
+if __name__ == '__main__':
+    if sys.argv[1] == 'add':
+        addItem()
+    else if sys.argv[1] == 'checkout'
+        return checkout()
+    else if sys.argv[1] == 'checkin'
+        return checkin()
+    else if sys.argv[1] == 'log'
+        return log()
+    else if sys.argv[1] == 'remove'
+        return remove()
+    else if sys.argv[1] == 'init'
+        return init()
+    else if sys.argv[1] == 'verify'
+        return verify()
+    return 0
